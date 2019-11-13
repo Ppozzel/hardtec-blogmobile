ada2b317a49ca548f38297a9691352df7b528798
Ppozzel <pedropozel@gmail.com>
2019-11-13 12:41:40 -0300

    splash artificial

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hardtec_app/homePage.dart';
 
+import 'pages/splash.dart';
+
 
 void main() {
   runApp(new MaterialApp(
-    home: NoticeHardTec(),
+    home: SplashScreen(),
     debugShowCheckedModeBanner: false,
+    routes: {"/homepage": (_) => NoticeHardTec()},
   ));
 }
